f630c117fc3a14b17c0e5e6338eaf0ad510be702
Thi-Ponce <thi.pponce@gmail.com>
2022-05-13 15:05:01 -0300

    list all books

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
 class App
+  include Inputs
   def initialize
     @persons = []
     @books = []
   end
   
   attr_accessor :persons, :books
+
+  def list_all_books
+    puts "\n"
+    books.each_with_index do |book, index|
+    puts "[#{index + 1}] Title: #{book.title}, Author: #{book.author}"
+    end
+  end
+
+  def list_all_persons
+  end
+
+  def create_person
+  end
+
+  def create_book
+  end
+
+  def create_rental
+  end
+
+  def list_rentals
+  end
 
 end 
